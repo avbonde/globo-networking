6e4f476eb459c04ebaa97ba804979ee220ed94d2
avbonde <bondeanna@gmail.com>
2024-08-14 14:55:15 +0200

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,49 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+/*
+import {
+  to = module.main.aws_vpc.this[0]
+  id = "vpc-0ddd8e02ab2456e9f" #VPC
+}
+
+import {
+  to = module.main.aws_subnet.public[0]
+  id = "subnet-0b64f53db73b733fe" #Public Subnet 1
+}
+
+import {
+  to = module.main.aws_subnet.public[1]
+  id = "subnet-051f63bafbc5f59f7" #Public Subnet 2
+}
+
+import {
+  to = module.main.aws_internet_gateway.this[0]
+  id = "igw-0bf94102d7a9739f0" #Internet gateway
+}
+
+import {
+  to = module.main.aws_route.public_internet_gateway[0]
+  id = "rtb-06aef9ee48bc3feb3_0.0.0.0/0" #Default public route
+}
+
+import {
+  to = module.main.aws_route_table.public[0]
+  id = "rtb-06aef9ee48bc3feb3" #Public Route Table
+}
+
+import {
+  to = module.main.aws_route_table_association.public[0]
+  id = "subnet-0b64f53db73b733fe/rtb-06aef9ee48bc3feb3" #Public Subnet 1/Public Route Table
+}
+
+import {
+  to = module.main.aws_route_table_association.public[1]
+  id = "subnet-051f63bafbc5f59f7/rtb-06aef9ee48bc3feb3" #Public Subnet 2/Public Route Table
+}
+
+import {
+  to = aws_security_group.ingress
+  id = "sg-0e5395547ccdd3d94" #NoIngressSecurityGroup
+}
+*/
